3b2f5b62912d2453e342dce2b92e81b8a93cca5b
Your Name <LukeNewman@amdg.rockhursths.edu>
2020-02-18 08:49:59 -0600

    cumquatg

--- cards.py
+++ cards.py
@@ -2,7 +2,7 @@ deck = []
 suits = ["♠", "♥", "♦", "♣"]
 
 for i in range(4):
-    for j in range(4, 14):
+    for j in range(1, 14):
         value = str(j)
         if(j == 1):
             value = "A"
@@ -16,6 +16,31 @@ for i in range(4):
 
 print(deck)
 
+#commit to github under siumulation
+
 # shuffle the deck
 
-# print the deck
+# print the shuffled deck
+#commit a second time to github
+# turn in two histories
+
+import random 
+  
+def randomize (deck, n): 
+    # Start from the last element and swap one by one. We don't 
+    # need to run for the first element that's why i > 0 
+    for i in range(n-1,0,-1): 
+        # Pick a random index from 0 to i 
+        j = random.randint(0,i+1) 
+  
+        # Swap arr[i] with the element at random index 
+        deck[i],deck[j] = deck[j],deck[i] 
+    return deck
+
+# Driver program to test above function. 
+
+n = len(deck)
+
+print(randomize(deck, n)) 
+  
+
